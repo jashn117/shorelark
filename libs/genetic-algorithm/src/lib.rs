526af1839ceafb320dde5335d8ec9c4598fd288c
jashn117 <jashn.singh117@gmail.com>
2022-04-12 23:33:28 +0530

    add chromosome

--- libs/genetic-algorithm/src/lib.rs
+++ libs/genetic-algorithm/src/lib.rs
@@ -1,3 +1,5 @@
+use std::ops::Index;
+
 use rand::RngCore;
 use rand::seq::SliceRandom;
 
@@ -37,6 +39,51 @@ impl SelectionMethod for RoulleteWheelSelection {
         population
             .choose_weighted(rng, |individual| individual.fitness())
             .expect("population cannot be zero")
+    }
+}
+
+pub struct Chromosome {
+    genes: Vec<f32>,
+}
+
+impl Chromosome {
+    pub fn len(&self) -> usize {
+        self.genes.len()
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item = &f32> {
+        self.genes.iter()
+    }
+
+    pub fn mut_iter(&mut self) -> impl Iterator<Item = &mut f32> {
+        self.genes.iter_mut()
+    }
+}
+
+impl Index<usize> for Chromosome {
+    type Output = f32;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.genes[index]
+    }
+}
+
+impl FromIterator<f32> for Chromosome {
+    fn from_iter<T: IntoIterator<Item = f32>>(iter: T) -> Self {
+        Self {
+            genes: iter
+                .into_iter()
+                .collect(),
+        }
+    }
+}
+
+impl IntoIterator for Chromosome {
+    type Item = f32;
+    type IntoIter = std::vec::IntoIter<f32>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.genes.into_iter()
     }
 }
 
